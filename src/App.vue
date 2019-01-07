d780c704f6bf65685db599c972b82a9f1a0b62fc
win <2954128748@qq.com>
2019-01-07 17:38:25 +0800

    引入sass

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+    <div class="adv"></div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import Vue from "vue";
+  // import "./sass/common.scss";
+  export default {
+    name: 'App'
+  }
 </script>
 
-<style>
-#app {
+<style lang="scss">
+@import "./sass/common.scss";
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+.adv{
+  height: 50px;
+  background: #f00;
 }
+
+
 </style>
