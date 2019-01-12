08f1e15ab3124edc761ccf8e118a576cdaa1cf6e
win <2954128748@qq.com>
2019-01-12 17:02:19 +0800

    01-12/17:02

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -119,7 +119,116 @@
 
         <!-- 华为MATE20 -->
         <div class="recommendtwo">
+            <a :href="item.link" v-for="item in recommendtwo" :key="item.id">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
 
+        <!-- iPhone使用技巧 -->
+        <div class="recommendthree">
+            <a :href="item.link" v-for="item in recommendthree" :key="item.id">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
+
+        <!-- MacBookAir -->
+        <div class="recommendfour">
+            <a :href="item.link" v-for="item in recommendfour" :key="item.id">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
+
+        <!-- 新人礼 -->
+        <div class="newgift">
+            <a :href="item.link" v-for="item in newgift" :key="item.id">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
+
+        <!-- 为你优选 -->
+        <div class="justforyou"> <!--图片-->
+            <img src="../assets/justforyou.png" alt="">
+        </div>
+        <div class="justyou"> <!--内容-->
+            <a :href="item.link" v-for="(item,idx) in justforyou" :key="idx">
+                <img :src="item.imagePath" alt="">
+                <p>{{item.title}}</p>
+                <p>{{item.sellingPoint}}</p>
+                <p>￥{{item.price}}</p>
+            </a>
+        </div>
+        
+        <!-- 手机精品 -->
+        <div class="boutiquepic"> <!--图片-->
+            <img src="../assets/boutiquephone.png" alt="">
+        </div>
+        <div class="boutiqueone">
+            <a :href="item.link" v-for="(item,idx) in boutiqueone" :key="idx">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
+        <div class="boutiquetwo">
+            <a :href="item.link" v-for="(item,idx) in boutiquetwo" :key="idx">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
+        <div class="boutiquethree">
+            <a :href="item.link" v-for="(item,idx) in boutiquethree" :key="idx">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
+
+        <!-- 办公专家 -->
+        <div class="officepic"> <!--图片-->
+            <img src="../assets/officer.png" alt="">
+        </div>
+        <div class="officeone">
+            <a :href="item.link" v-for="(item,idx) in officeone" :key="idx">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
+        <div class="officetwo">
+            <a :href="item.link" v-for="(item,idx) in officetwo" :key="idx">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
+        <div class="officethree">
+            <a :href="item.link" v-for="(item,idx) in officethree" :key="idx">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
+
+        <!-- 智能生活 -->
+        <div class="lifepic"> <!--图片-->
+            <img src="../assets/life.png" alt="">
+        </div>
+        <div class="lifeone">
+            <a :href="item.link" v-for="(item,idx) in lifeone" :key="idx">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
+        <div class="lifetwo">
+            <a :href="item.link" v-for="(item,idx) in lifetwo" :key="idx">
+                <img :src="item.imagePath" alt="">
+            </a>
+        </div>
+
+        <!-- 发现好货 -->
+        <div class="findpic"> <!--图片-->
+            <img src="../assets/findgood.png" alt="">
+        </div>
+        <div class="findgood"> <!--内容-->
+            <a :href="item.link" class="goods" v-for="(item,idx) in findgood" :key="idx">
+                <div class="goodspic"> <!--图片-->
+                    <img :src="item.imagePath" alt="">
+                    <img :src="item.promotionTagImg" alt="">
+                </div>
+                <div class="goodscon"> <!--内容-->
+                    <p>{{item.sellingPoint}}</p>
+                    <p>{{item.title}}</p>
+                    <p>￥{{item.price}}</p>
+                </div>
+            </a>
         </div>
     </div>
 </template>
@@ -190,9 +299,35 @@
                 recommendone:[], //iPoneXR全网通
 
                 recommendtwo:[], //华为MATE20
+
+                recommendthree:[], //iPhone使用技巧
+
+                recommendfour:[], //MacBookAir
+
+                newgift:[], //新人礼
+
+                justforyou:[], //为你优选
+
+                boutiqueone:[], //手机精品1
+                boutiquetwo:[], //手机精品2
+                boutiquethree:[], //手机精品3
+
+                officeone:[], //办公专家1
+                officetwo:[], //办公专家2
+                officethree:[], //办公专家3
+
+                lifeone:[], //智能生活1
+                lifetwo:[], //智能生活2
+
+                findgood:[], //发现好货
                 
             }
         },
+        // computed:{
+        //     filterjustforyou: function(){
+        //         return this.justforyou.filter(item=>item.type!=0)
+        //     }
+        // },
         methods: {
             selected(path){
                 this.active = path;
@@ -210,9 +345,9 @@
                 const that = this;
                 // console.log(that);
                 setTimeout(function () {
-                that.countdown()
+                    that.countdown()
                 }, 1000)
-            }
+            },
         },
         mounted: function () {
             //限时抢购倒计时
@@ -238,11 +373,28 @@
                 this.headline = res.data.data.container.floor[4].content;
                 this.recommendone = res.data.data.container.floor[5].content;
                 this.recommendtwo = res.data.data.container.floor[6].content;
-
+                this.recommendthree = res.data.data.container.floor[7].content;
+                this.recommendfour = res.data.data.container.floor[8].content;
+                this.newgift = res.data.data.container.floor[9].content;
+                this.boutiqueone = res.data.data.container.floor[13].content;
+                this.boutiquetwo = res.data.data.container.floor[14].content;
+                this.boutiquethree = res.data.data.container.floor[15].content;
+                this.officeone = res.data.data.container.floor[17].content;
+                this.officetwo = res.data.data.container.floor[18].content;
+                this.officethree = res.data.data.container.floor[19].content;
+                this.lifeone = res.data.data.container.floor[21].content;
+                this.lifetwo = res.data.data.container.floor[22].content;
+                this.findgood = res.data.data.container.floor[25].content;
             })
             Axios.get('/dbapi/floors/specialEnjoy/v1').then(res=>{
-                // console.log(res.data.data.rushToday);
+                // console.log(res.data.data.optimize);
                 this.rushtoday = res.data.data.rushToday;
+
+                for(var i=0;i<res.data.data.optimize.length;i++){
+                    if(res.data.data.optimize[i].type !=0){
+                        this.justforyou[i] = res.data.data.optimize[i];
+                    }
+                }
             })
         }
     }
@@ -436,15 +588,15 @@
                     width: 105.6rem;
                     height: 3.733333rem;
                     padding: 0 .133333rem;
-                    overflow: hidden;
+                    overflow: auto;
                     display: flex;
                     align-items: center;
                     .timebuyri{
                         // width: 105.6rem;
-                        height: 3.733333rem;
+                        // height: 3.733333rem;
                         display: flex;
                         align-items: center;
-                        overflow: hidden;
+                        overflow: auto;
                         z-index: 1;
                         >a{
                             width: 2.933333rem;
@@ -531,7 +683,257 @@
 
         //华为MATE20
         .recommendtwo{
+            height: 3.333333rem;
+            display: flex;
+            img{
+                height: 3.333333rem;
+            }
+        }
 
+        // iPhone使用技巧
+        .recommendthree{
+            height: 1.666667rem;
+            display: flex;
+            img{
+                height: 1.666667rem;
+            }
+        }
+
+        //MacBookAir
+        .recommendfour{
+            height: 1.666667rem;
+            display: flex;
+            img{
+                height: 1.666667rem;
+            }
+        }
+
+        //新人礼
+        .newgift{
+            margin-top: .266667rem;
+            height: 1.666667rem;
+            display: flex;
+            img{
+                height: 1.666667rem;
+            }
+        }
+
+        //为你优选
+        .justforyou{ //图片
+            // width: 100%;
+            height: 1.44rem;
+            margin-top: .266667rem;
+            overflow: hidden;
+            position: relative;
+            img{
+                height: 1.44rem;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+            }
+        }
+        .justyou{ //内容
+            // height: 5.669333rem;
+            // width: 23.733333rem;
+            overflow:auto;
+            // background: #09a;
+            padding: .266667rem;
+            display: flex;
+            a{
+                width: 3.2rem;
+                padding: .266667rem;
+                margin-right: .266667rem;
+                box-shadow: 0 0 .266667rem #e1e1e1;
+                border-radius: .08rem;
+                text-align: center;
+                >img{
+                    width: 3.2rem;
+                    height: 3.2rem;
+                }
+                >p:nth-child(2){
+                    font-size: .346667rem;
+                    color:#333;
+                    margin-top: .08rem;
+                    text-overflow:ellipsis;
+                    overflow:hidden;
+                    white-space:nowrap;
+                }
+                >p:nth-child(3){
+                    font-size: .293333rem;
+                    color: #9c9c9c;
+                    margin-top: .08rem;
+                }
+                >p:nth-child(4){
+                    font-size: .373333rem;
+                    color: #f21c1c;
+                    margin-top: .08rem;
+                }
+            }
+        }
+
+        //手机精品
+        .boutiquepic{ //图片
+            height: 1.44rem;
+            margin-top: .266667rem;
+            overflow: hidden;
+            position: relative;
+            img{
+                height: 1.44rem;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+            }
+        }
+        .boutiqueone{
+            display: flex;
+            height: 3.333333rem;
+            img{
+                height: 3.333333rem;
+            }
+        }
+        .boutiquetwo{
+            display: flex;
+            height: 3.333333rem;
+            img{
+                height: 3.333333rem;
+            }
+        }
+        .boutiquethree{
+            display: flex;
+            height: 1.666667rem;
+            img{
+                height: 1.666667rem;
+            }
+        }
+
+        //办公专家
+        .officepic{ //图片
+            height: 1.44rem;
+            overflow: hidden;
+            position: relative;
+            img{
+                height: 1.44rem;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+            }
+        }
+        .officeone{
+            display: flex;
+            height: 3.333333rem;
+            img{
+                height: 3.333333rem;
+            }
+        }
+        .officetwo{
+            display: flex;
+            height: 3.333333rem;
+            img{
+                height: 3.333333rem;
+            }
+        }
+        .officethree{
+            display: flex;
+            height: 1.666667rem;
+            img{
+                height: 1.666667rem;
+            }
+        }
+
+        //智能生活
+        .lifepic{ //图片
+            height: 1.44rem;
+            overflow: hidden;
+            position: relative;
+            img{
+                height: 1.44rem;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+            }
+        }
+        .lifeone{
+            display: flex;
+            height: 3.333333rem;
+            img{
+                height: 3.333333rem;
+            }
+        }
+        .lifetwo{
+            display: flex;
+            height: 1.666667rem;
+            img{
+                height: 1.666667rem;
+            }
+        }
+
+        //发现好货
+        .findpic{ //图片
+            height: 1.44rem;
+            overflow: hidden;
+            position: relative;
+            img{
+                height: 1.44rem;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+            }
+        }
+        .findgood{ //内容
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            .goods{
+                width: 4.6rem;
+                background: #fff;
+                margin-top: .3rem;
+                padding-bottom: .266667rem;
+                box-shadow: 0 .026667rem .266667rem #e1e1e1;
+                border-radius: .08rem;
+                .goodspic{
+                    position: relative;
+                    img:nth-child(1){
+                        width: 4rem;
+                        height: 4rem;
+                        margin: .266667rem;
+                    }
+                    img:nth-child(2){
+                        width: 1.2rem;
+                        position: absolute;
+                        top: 3.466667rem;
+                        left:0;
+                    }
+                }
+                .goodscon{
+                    p:nth-child(1){
+                        height: .682667rem;
+                        font-size: .373333rem;
+                        color: #87acd8;
+                        background: #ebf6fe;
+                        padding: .133333rem .4rem;
+                        line-height: .416rem;
+                    }
+                    p:nth-child(2){
+                        text-overflow:ellipsis;
+                        overflow:hidden;
+                        white-space:nowrap;
+                        margin: .133333rem .4rem;
+                        height: .448rem;
+                        font-size: .373333rem;
+                        color: #333;
+                    }
+                    p:nth-child(3){
+                        margin: .133333rem .4rem 0;
+                        color: #f21c1c;
+                        font-size: .4rem;
+                    }
+                }
+            }
         }
     }
 </style>
