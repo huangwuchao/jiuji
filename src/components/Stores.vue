ef408d5fb49e866e0b84ff0a649ac48a397fe1c2
win <2954128748@qq.com>
2019-01-14 13:50:02 +0800

    01-14/13:49

--- src/components/Stores.vue
+++ src/components/Stores.vue
@@ -1,8 +1,39 @@
 <template>
-    <div>天河区</div>
+    <div id="Stores">
+        <div class="stotop"><!-- 顶部框 -->
+            <a href="javascript:;" @click="goback" class="fa fa-angle-left"></a>
+            <div></div>
+            <a href="javascript:;"></a>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-    
+    methods:{
+        //返回上一页
+        goback(){
+            this.$router.go(-1);
+        },
+    },
 }
 </script>
+<style lang="scss">
+    #Stores{
+        flex:1;
+        overflow-x: hidden;
+        .stotop{
+            height: 1.173333rem;
+            background: #f21c1c;
+            display: flex;
+            // align-items: center;
+            >a:nth-child(1){
+                width: 1.173333rem;
+                height: 1.173333rem;
+                font-size: .8rem;
+                color: #fff;
+                text-align: center;
+                line-height: 1.173333rem;
+            }
+        }
+    }
+</style>
